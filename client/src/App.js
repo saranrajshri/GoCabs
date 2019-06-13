561101371ed60eb97fd23ba22b1b80af3352e1ad
saranrajshri <shrisaranraj2001@gmail.com>
2019-06-14 01:01:54 +0530

    initial commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
